bd81e16357e11440daba58809cf366941420574e
Nataly-Costaa <natalynaty653@gmail.com>
2025-04-19 18:22:19 -0300

    :recycle: refactor: Modificando o nome da rota do post de cadastra para register

--- src/routes/habit.routes.js
+++ src/routes/habit.routes.js
@@ -5,7 +5,7 @@ const habits = Router();
 
 habits.get("/", HabitController.getHabits);
 habits.get("/report", HabitController.habitReport);
-habits.post("/cadastra", HabitController.createHabit);
+habits.post("/register", HabitController.createHabit);
 habits.patch("/:id/complete", HabitController.markHabitCompleted);
 habits.delete("/:id", HabitController.deleteHabit);
 
