3b0a2e1c9adc7d0ce154472e7a6eadabd3e12cc5
Nataly-Costaa <natalynaty653@gmail.com>
2025-06-28 00:35:46 -0300

    :recycle: refactor: Mudando a posição das rotas

--- src/routes/habit.routes.js
+++ src/routes/habit.routes.js
@@ -3,9 +3,9 @@ import { HabitController } from '../controller/habit.controller.js';
 
 const router = Router();
 
+router.get('/report', HabitController.report);
 router.get('/', HabitController.getHabits);
 router.get('/:id', HabitController.getHabitById);
-router.get('/report', HabitController.report);
 router.post('/register', HabitController.createHabit);
 router.patch('/:id/complete', HabitController.markCompleted);
 router.delete('/:id', HabitController.deleteHabit);
